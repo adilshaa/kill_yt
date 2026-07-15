957e20d081d3c395e5018ccdb7c4cbfa4642bc71
iillcode <illcode.business@gmail.com>
2026-07-15 23:54:52 +0530

    chore: project config, dark mobile theme, env template, SW mount

---
+++ components/ServiceWorkerRegister.tsx
@@ -0,0 +1,17 @@
+"use client";
+
+import { useEffect } from "react";
+
+export default function ServiceWorkerRegister() {
+  useEffect(() => {
+    if ("serviceWorker" in navigator) {
+      navigator.serviceWorker
+        .register("/sw.js", { scope: "/", updateViaCache: "none" })
+        .catch(() => {
+          /* registration failures are non-fatal */
+        });
+    }
+  }, []);
+
+  return null;
+}
